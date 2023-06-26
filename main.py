383feb1fbf46d9e7340158f6c95446e220d5e132
shubhamiitb123 <shubhamiitb123@gmail.com>
2023-06-26 09:41:32 +0530

    first commit for streramlit project

---
+++ main.py
@@ -0,0 +1,206 @@
+import streamlit as st
+
+import os
+
+import requests
+import pandas as pd 
+import numpy as np
+from bs4 import BeautifulSoup
+import pypdf
+import llama_index
+import os
+
+with open('key.txt','r') as f:
+
+    secret_key=f.read().strip()
+
+
+
+
+
+
+from views import get_chat_response,company_with_url,report_url,autodownload_report,pdf2txt,create_index,answerMe,balance,shareholding,display_pdf,load_index,give_answer
+Base_Dir=os.getcwd()
+st.header('Financial Bot')
+
+query=st.text_input('***tell name of company***')
+
+
+
+
+
+
+press=st.button('click here')
+if press:
+
+    st.write('confirm query :  ',query)
+
+
+    company_name=get_chat_response(query+'\n \n give acutal name of this company in only one or two words ')
+
+    st.write('company name :',company_name)
+    full_company_name,screener_url=company_with_url(company_name)
+    
+
+    st.write('company from our database:',full_company_name)
+
+    company=full_company_name.split(' ')[0]
+
+    import time
+
+    time.sleep(10)
+
+
+    # st.write('***if you have annual report of it then you can upload it***')
+    # yes_no=st.button('Yes')
+    # not_pdf=st.button('No')
+  
+
+    if None:
+        import base64
+
+        def download_file(file,company):
+            """Downloads a file to a local folder."""
+            filepath=os.path.join(Base_Dir,f'{company}_2022.pdf')
+            with open(filepath, "wb") as f:
+                f.write(file.read())
+            return filepath
+
+        uploaded_file = st.file_uploader("Upload a file", type=["pdf"])
+        if uploaded_file is not None:
+            pdf_file_path = download_file(uploaded_file,company)
+
+
+
+
+
+    else:
+
+
+        st.write('Url of company:',screener_url)
+        
+        
+
+        st.write('Balance Sheet from Screener Website:')
+        st.write(balance(screener_url))
+
+        st.write('shareholding from Screener Website:')
+        st.write(shareholding(screener_url))
+
+
+        annual_report_url=report_url(screener_url)
+
+        st.write(f'link of annual report 2022 of {full_company_name}',annual_report_url)
+
+        
+
+        st.write('annual report download stared ...')
+        autodownload_report(annual_report_url,company)
+
+        st.write('download successful')
+
+
+    
+    
+
+    pdf_path=os.path.join(Base_Dir,f'{company}_2022.pdf')
+    
+    
+
+    pdf2txt(pdf_path,company)
+
+    st.write('report deleted and important data is stored in txt file ')
+
+    st.write('this is pages of anuual report that contains important data')
+
+    # pdf_display=display_pdf(f'{company}.pdf')
+    import base64
+    def display_pdf(pdf_file):
+        with open(pdf_file, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+        return 
+    display_pdf(f'{company}_imp.pdf')
+
+    import openai
+
+    openai.api_key = secret_key
+
+
+    
+    st.write('creating vector store and storing data in it')
+    create_index(company)
+
+    st.write('vector store created succesfully ! ')
+
+
+
+    import re
+    def extract_number(string):
+        """Extracts the number from the string."""
+        regex = r"\d+,\d{3}"
+        match = re.search(regex, string)
+        if match:
+            return match.group(0)
+        else:
+            return None
+
+    questions = [
+        "please read the Balance Sheet provided in pdf and fetch the value of Total non - current assets for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total current assets for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total assets for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total equity for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total non-current liabilities for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total current liabilities for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total equity and liabilities for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total income for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total expenses for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Profit before tax . fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Profit for the year for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of Total comprehensive income for the year for Mar 2023. fetch only value",
+        "please read the Balance Sheet provided in pdf and fetch the value of tax applied for the year for Mar 2023. fetch only value"
+    ]
+
+    vstore_path=os.path.join(Base_Dir,f'vector_stores/{company}_vstore')
+
+    index=load_index(vstore_path)
+
+
+    data_dict=dict()
+    key=['total non-current assets','total current assests','total asset for march','total equity','Total non-current liabilities','Total current liabilities','Total equity and liabilities','Total income','Total expenses','Profit before tax','Profit for the year','comprehensive income','tax']
+    for i in range(len(questions)):
+        ans=give_answer(index,questions[i]).response
+        
+        
+        if ans:
+            
+            ans=ans.replace('\n','')
+            data_dict[key[i]]=ans
+            st.write(f'{key[i]} => {ans} ')
+        else:
+            data_dict[key[i]]=ans
+
+            st.write(f'{key[i]} => {ans} ')
+
+    def final_func(sen):
+    
+        ans=''
+        for i in sen:
+            if i.isdigit() or i==',':
+                ans+=i
+        return ans
+    
+    df=pd.DataFrame({f'{company}':data_dict.keys(),'2022':data_dict.values()})
+    df['2022']=df['2022'].apply(final_func)
+
+
+    st.write(df)
+
+    csv_data = df.to_csv(index=False)
+    st.download_button(label='Download CSV', data=csv_data, file_name=f'{company}_data.csv', mime='text/csv')
+
+
+
+
+st.write('thanks for visiting us !!!')
---
+++ views.py
@@ -0,0 +1,249 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import os
+from llama_index import SimpleDirectoryReader,GPTListIndex,GPTVectorStoreIndex,LLMPredictor,PromptHelper,ServiceContext,StorageContext
+from langchain import OpenAI
+
+import openai
+import llama_index
+
+# from main import secret_key
+with open('key.txt','r') as f:
+
+    secret_key=f.read().strip()
+os.environ["OPENAI_API_KEY"]=secret_key
+from langchain import OpenAI
+
+Base_Dir=os.getcwd()
+
+from PyPDF2 import PdfReader,PdfWriter
+
+
+
+def get_chat_response(question):
+    # API endpoint
+    url = 'https://api.openai.com/v1/chat/completions'
+
+    # Your OpenAI API key
+    api_key = secret_key
+
+    # Request headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    }
+
+    # Request payload
+    payload = {
+        'model': 'gpt-3.5-turbo',
+        'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                     {'role': 'user', 'content': question}]
+    }
+
+    # Send POST request to the API
+    response = requests.post(url, headers=headers, json=payload)
+
+    # Parse the response
+    data = response.json()
+    
+    reply= data['choices'][0]['message']['content']
+
+    # Get the model's reply
+#     reply = data['choices'][0]['message']['content']
+
+    return reply
+
+
+def company_with_url(company_name):
+    csv_path=os.path.join(Base_Dir,'final_compnaies.csv')
+    from sentence_transformers import SentenceTransformer,util
+    encode_model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    df=pd.read_csv(r"C:\Users\Shubham\Downloads\companies_data.csv")
+    companies=list(df['company'])
+    companies_urls=list(df['screener url'])
+    encoded_names=encode_model.encode(companies)
+
+    cos=util.cos_sim(encode_model.encode(company_name.split()[0]),encoded_names)
+
+        
+    m=0
+    index=0
+    for i in range(len(cos[0])):
+
+        if m<cos[0][i].item():
+            index=i
+            m=cos[0][i]
+
+    company=companies[index]
+    screener_url=companies_urls[index]
+
+    return (company,screener_url)
+
+
+def report_url(url):
+
+    soup_annual=BeautifulSoup(requests.get(url).content,'html.parser')
+    annual_urls=[i.get('href') for i in soup_annual.find_all('a')]
+
+    annual_reports=[]
+    for i in annual_urls:
+        if 'Annual' in i and '#' not in i:
+            annual_reports.append(i)
+
+    annual_report_2022=annual_reports[0]
+
+    return annual_report_2022
+
+
+
+
+
+
+
+def autodownload_report(url,company):
+
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36'
+    }
+    
+    response = requests.get(url, stream=True,headers=headers)
+
+    folder_path=os.path.join(Base_Dir,f'Annual_reports/{company}_report')
+
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+        print('folder created')
+        pdf_path=os.path.join(Base_Dir,f'{company}_2022.pdf')
+        # print(pdf_path)
+        with open(pdf_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                f.write(chunk)
+        
+    return 
+
+
+
+def pdf2txt(pdf_path,company):
+
+    if not os.path.exists(os.path.join(Base_Dir,f'Annual_reports/{company}_report/{company}_2022.txt')):
+
+        titles = ['STANDALONE BALANCE SHEET', 'STANDALONE STATEMENT OF PROFIT AND LOSS', 'Balance Sheet', 'Balance Sheet (contd.)', 'Statement of Profit and Loss', 'Statement of Profit and Loss (contd.)']   
+        with open(pdf_path, 'rb') as pdf_file:
+                # Create a PDF reader object
+                pdf_reader = PdfReader(pdf_file)
+                text=''
+                pdf_writer = PdfWriter()
+                page_no=0
+
+                for page in pdf_reader.pages:
+
+                    page_content=page.extract_text()
+                    page_no+=1
+                    for word in titles:
+                        if word in page_content:
+                            # print(page_no)
+                            text+=page.extract_text()
+                            pdf_writer.add_page(page)
+        
+        with open(f'{company}_imp.pdf', 'wb') as output_file:
+            pdf_writer.write(output_file)
+
+        txt_path=os.path.join(Base_Dir,f'Annual_reports/{company}_report/{company}_2022.txt')
+        with open(txt_path,'w',encoding='utf-8') as f:
+            f.write(text)
+        print('created txt file')
+        pdf_path=os.path.join(Base_Dir,f'{company}_2022.pdf')
+        os.remove(pdf_path)
+
+        print('removed pdf file')
+
+    return 
+
+import base64
+def display_pdf(pdf_file):
+    with open(pdf_file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf"></iframe>'
+    
+    return pdf_display
+
+def create_index(company):
+
+    import openai
+
+    # api_key = "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"
+    openai.api_key = secret_key
+
+
+    
+    vstore_path=os.path.join(Base_Dir,f'vector_stores/{company}_vstore')
+    doc_path=os.path.join(Base_Dir,f'Annual_reports/{company}_report')
+    if not os.path.exists(vstore_path):
+
+        os.mkdir(vstore_path)
+        max_input=4096
+        tokens=200
+        chunk_size=600
+        max_chunk_overlap=20
+        
+        promptHelpter=PromptHelper(max_input,max_chunk_overlap,chunk_size_limit=chunk_size)
+        
+        openai.api_key=secret_key
+        llmPredictor=LLMPredictor(llm=OpenAI(temperature=0,model_name='text-ada-001',max_tokens=tokens))
+        
+        docs=SimpleDirectoryReader(doc_path).load_data()
+        
+        service_context=ServiceContext.from_defaults(llm_predictor=llmPredictor,prompt_helper=promptHelpter)
+        openai.api_key=secret_key
+        vectorIndex=GPTVectorStoreIndex.from_documents(documents=docs)
+        
+
+        
+
+        vectorIndex.storage_context.persist(persist_dir=vstore_path)
+
+    return 
+
+
+def load_index(vstore_path):
+
+
+# rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir=vstore_path)
+    # load index
+    index = llama_index.load_index_from_storage(storage_context)
+
+    return index
+
+# print(index)
+
+def give_answer(index,que):
+
+    return index.as_query_engine().query(que)
+
+
+def answerMe(question,company):
+
+    vstore_path=os.path.join(Base_Dir,f'vector_stores/{company}_vstore')
+    storage_context=StorageContext.from_defaults(persist_dir=vstore_path)
+#     index=load_index_from_storage(storage_context)
+    index=llama_index.load_index_from_storage(storage_context)
+    query_engine=index.as_query_engine()
+    response=query_engine.query(question)
+    
+    return response.response
+
+
+def balance(url):
+    dfs = pd.read_html(url)
+
+    return dfs[6]
+def shareholding(url):
+    dfs = pd.read_html(url)
+
+    return dfs[10]
+def balance(url):
+    dfs = pd.read_html(url)
+
+    return dfs[6]
